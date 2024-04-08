43e7f6d55db0ee476c982fde4ca3e1b1b50aa7fc
sachinv26 <vermas7008@gmail.com>
2024-04-08 09:21:30 +0530

    spell change

--- lib/clients/dashboard/home/branch_therapy.dart
+++ lib/clients/dashboard/home/branch_therapy.dart
@@ -76,7 +76,7 @@ class _BranchTherapiesState extends State<BranchTherapies> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    'Therapists List',
+                    'Therapists',
                     style: kTextStyle1,
                   ),
                   Text(
